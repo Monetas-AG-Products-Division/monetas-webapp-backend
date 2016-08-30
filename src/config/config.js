89bf25c1690f558ed9d1b4c045dd59b16ed699aa
Vadim <vadim.inc@gmail.com>
2016-08-30 21:42:01 +0200

    add prod config

--- src/config/config.js
+++ src/config/config.js
@@ -53,7 +53,7 @@ var config = {
     }
   },
   production: {
-    db: process.env.MONGODB || 'localhost:27017/monetas',
+    db: process.env.MONGODB || 'dbuser:monetaspassword@ds019634.mlab.com:19634/monetas-staging',
     port: process.env.PORT || 3000,
     root: rootPath,
     secret: {
